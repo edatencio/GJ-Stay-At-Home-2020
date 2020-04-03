66732369890921903ca95a0cf859872362493736
Edward Atencio <ed.atencio16@gmail.com>
2020-04-03 13:20:15 -0500

    Imported autosave

---
+++ Assets/Plugins/Autosave/Editor/Autosave.cs
@@ -0,0 +1,23 @@
+﻿//Source: https://gist.github.com/edatencio/7ddb1410e92d11617ad333c9efa7ef83/
+//Original: https://forum.unity3d.com/threads/we-need-auto-save-feature.483853/
+
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class Autosave
+{
+    static Autosave()
+    {
+        EditorApplication.playModeStateChanged += (PlayModeStateChange state) =>
+        {
+            if (state == PlayModeStateChange.ExitingEditMode)
+            {
+                Debug.Log("Auto-saving all open scenes...");
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
