d5f7629e63ae5b163973c7e2974007164cf11079
hedersonleeh <hederson_3_12@hotmail.com>
2020-04-04 19:29:42 -0500

    AministrationPanelImplemented

---
+++ Assets/Scripts/ResultPanel.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ResultPanel : MonoBehaviour
+{
+    public TextMeshProUGUI displaySatisfaction;
+    public TextMeshProUGUI displayCostumers;
+    public TextMeshProUGUI displayMoney;
+    private void OnEnable()
+    {
+      displaySatisfaction.text = Restaurant.instance?.SatisfactionTotal.ToString();   
+      displayMoney.text = "TODO";   
+      displayCostumers.text = Restaurant.instance?.clientCount.ToString("D2");   
+    }
+
+}
