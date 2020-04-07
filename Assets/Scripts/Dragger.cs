d1d50920ffd6b5322250b1b6bea24d76d18bb707
Edward Atencio <ed.atencio16@gmail.com>
2020-04-06 21:18:00 -0500

    Fixed dragger

--- Assets/Scripts/Dragger.cs
+++ Assets/Scripts/Dragger.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Dragger : MonoBehaviour
 {
+    [SerializeField] private LayerMask groundLayer;
     private new Camera camera;
 
     public bool CanDrag { get; set; }
@@ -18,7 +19,7 @@ public class Dragger : MonoBehaviour
 
         if (Input.GetButton("Fire1"))
         {
-            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, camera.farClipPlane))
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, camera.farClipPlane, groundLayer))
                 transform.position = hit.point.With(y: transform.position.y);
         }
     }
